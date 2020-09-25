75fb535360a2a96079f2a361933899e453fe5b04
sebrink <se.brink15@gmail.com>
2020-09-25 00:54:48 -0500

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -4,7 +4,7 @@
 
 sudo apt update -y
 sudo apt install software-properties-common -y
-sudo add-apt-repository ppa:deadsnakes/ppa
+sudo add-apt-repository ppa:deadsnakes/ppa --force-yes
 sudo apt update -y
 # I'm sure I don't need something here, but I can't be bothered to fix it down, I don't care.
 sudo apt install python3.8 git openssh-server python3-setuptools rdate vim curl nmap libssl-dev libffi-dev python-dev build-essential python3.8-distutils python3.8-venv python3.8-dev -y 
