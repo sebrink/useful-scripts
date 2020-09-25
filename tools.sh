c4a668665ea04db8f89f14fbaf82dd729acc6415
sebrink <se.brink15@gmail.com>
2020-09-25 00:57:27 -0500

    you can't live without tmux

--- tools.sh
+++ tools.sh
@@ -7,7 +7,7 @@ sudo apt install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa --force-yes
 sudo apt update -y
 # I'm sure I don't need something here, but I can't be bothered to fix it down, I don't care.
-sudo apt install python3.8 git openssh-server python3-setuptools rdate vim curl nmap libssl-dev libffi-dev python-dev build-essential python3.8-distutils python3.8-venv python3.8-dev -y 
+sudo apt install python3.8 git openssh-server python3-setuptools tmux rdate vim curl nmap libssl-dev libffi-dev python-dev build-essential python3.8-distutils python3.8-venv python3.8-dev -y 
 echo "alias python3='/usr/bin/python3.8'" >> ~/.bashrc
 echo "alias python='/usr/bin/python3.8'" >> ~/.bashrc
 sudo python3.8 -m easy_install pip
